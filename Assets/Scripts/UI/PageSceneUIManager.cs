f0504b690ee2e55042f2de652bad6b76e24afb7e
Chapa.C(Takuya) <chapatown@gmail.com>
2018-06-03 02:04:41 +0900

    PageUI管理クラス作成

---
+++ Assets/Scripts/UI/PageSceneUIManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public class PageSceneUIManager : SingletonMonoBehaviour<PageSceneUIManager>
+    {
+        #region variables
+        [SerializeField]
+        private UIPageStatusBar statusBar;
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/UIPageStatusBar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using UnityEngine.UI;
+
+namespace CCG
+{
+    public class UIPageStatusBar : MonoBehaviour
+    {
+        #region variables
+        [SerializeField]
+        private Text goldText;
+        #endregion
+
+        #region public methods
+        public void UpdateGoldText(int gold)
+        {
+            goldText.text = $"{gold}G";
+        }
+        #endregion
+    }
+}
