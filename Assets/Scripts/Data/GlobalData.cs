4729b8d90f521ac7742b5e8bafa5033fa587f711
Chapa.C(Takuya) <chapatown@gmail.com>
2018-06-03 01:45:35 +0900

    GlobalData追加

---
+++ Assets/Scripts/Data/GlobalData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public static class GlobalData
+    {
+        #region properties
+        public static int totalGold { get; private set; }
+        public static int currentGold { get; private set; }
+        #endregion
+
+        #region public methods
+        public static void AddGold(int add)
+        {
+            totalGold += add;
+            currentGold += add;
+        }
+        #endregion
+    }
+}
