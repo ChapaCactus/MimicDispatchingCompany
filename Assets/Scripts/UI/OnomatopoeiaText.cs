8479a8bdba24729be1527d2d9ba940399fc1e89e
Chapa.C(Takuya) <chapatown@gmail.com>
2018-06-14 00:37:34 +0900

    擬音テキスト追加

---
+++ Assets/Scripts/UI/OnomatopoeiaText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using UnityEngine.UI;
+
+namespace CCG
+{
+    public class OnomatopoeiaText : MonoBehaviour
+    {
+    }
+}
