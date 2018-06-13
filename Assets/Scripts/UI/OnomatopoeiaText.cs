fdb6bfbb101dd009a46a22fdffbffd4e45548d4f
Chapa.C(Takuya) <chapatown@gmail.com>
2018-06-14 00:51:17 +0900

    擬音テキスト更新

--- Assets/Scripts/UI/OnomatopoeiaText.cs
+++ Assets/Scripts/UI/OnomatopoeiaText.cs
@@ -7,7 +7,45 @@ using UnityEngine.UI;
 
 namespace CCG
 {
-    public class OnomatopoeiaText : MonoBehaviour
+    public class OnomatopoeiaText
     {
+        #region properties
+        private Text text { get; set; }
+        #endregion
+
+        #region public methods
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public OnomatopoeiaText(Text text)
+        {
+            this.text = text;
+
+            Assert.IsNotNull(this.text);
+        }
+
+        public void SetMessage(string message)
+        {
+            text.text = message;
+        }
+
+        public void SetPosition(Vector2 position)
+        {
+            text.transform.position = position;
+        }
+
+        public void SetLocalPosition(Vector2 localPosition)
+        {
+            text.transform.localPosition = localPosition;
+        }
+
+        public void SetParent(Transform parent, bool worldPositionStays)
+        {
+            text.transform.SetParent(parent, worldPositionStays);
+        }
+        #endregion
+
+        #region private methods
+        #endregion
     }
 }
